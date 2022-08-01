c3aa4d5c56c9a911b2f84f6bc4be38f77cd7cc9b
phyohtetaung-dev <phyohtetaung.dev.29@gmail.com>
2022-08-01 23:07:09 +0630

    Add User Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            [
+                'name' => 'admin1',
+                'email' => 'admin1@gmail.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'admin2',
+                'email' => 'admin2@gmail.com',
+                'password' => Hash::make('password'),
+            ],
+        ]);
     }
 }
